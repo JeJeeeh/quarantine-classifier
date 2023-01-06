1e024a4c3b83dd8ebe57874367133cb41e55ef60
jje <jasonjonathan132@gmail.com>
2023-01-06 20:30:56 +0700

    victor jagoan

--- main.py
+++ main.py
@@ -9,65 +9,68 @@ class Cases(Fact):
 
 
 class NewDailyCases(Fact):
-    pass
+    count = Field(int, default=0)
+    status = Field(int, default=1)
 
 
 class AvailableHospitalBed(Fact):
-    available = Field(int, default=0)
-    occupied = Field(int, default=0)
+    availableRatio = Field(int, default=0)
 
 
 class QuarantineClassifier(KnowledgeEngine):
-    # death rate = death / confirmed * 100%
-    # death rate = 0% [0]
+
     @Rule(AND(
-        Cases(deathRate=0),
-        Cases(healedRate=90)
+        NewDailyCases(count=0),
+        NewDailyCases(status=1),
+        Cases(deathRate=0)
     ))
     def level0(self):
         print("Quarantine Level 0")
 
-    # death rate <= 10% [1]
-    @Rule(AND(
-        Cases(deathRate=10),
-        Cases(healedRate=80)
+    @Rule(OR(
+        Cases(deathRate=0),
+        AND(
+            Cases(healedRate=90),
+            NewDailyCases(status=1),
+            AvailableHospitalBed(availableRatio=90)
+        )
     ))
     def level1(self):
         print("Quarantine Level 1")
 
-    # death rate <= 20% [2]
-    @Rule(AND(
-        Cases(deathRate=20),
-        Cases(healedRate=70)
+    @Rule(OR(
+        Cases(deathRate=5),
+        AND(
+            Cases(healedRate=75),
+            NewDailyCases(status=1),
+            AvailableHospitalBed(availableRatio=75)
+        )
     ))
     def level2(self):
         print("Quarantine Level 2")
 
-    # death rate <= 40% [3]
-    @Rule(AND(
-        Cases(deathRate=40),
-        Cases(healedRate=50)
+    @Rule(OR(
+        Cases(deathRate=10),
+        Cases(healedRate=40),
+        NewDailyCases(status=0),
+        OR(
+            AvailableHospitalBed(availableRatio=30),
+            AvailableHospitalBed(availableRatio=0)
+        )
     ))
     def level3(self):
         print("Quarantine Level 3")
 
-    # death rate > 40% [4]
-    @Rule(AND(
+    @Rule(OR(
         Cases(deathRate=100),
-        Cases(healedRate=0)
+        AND(
+            Cases(healedRate=0),
+            NewDailyCases(status=0),
+            AvailableHospitalBed(availableRatio=0)
+        )
     ))
-    def level4(self):
+    def level3(self):
         print("Quarantine Level 4")
-
-    # healed rate = healed / hospitalized * 100
-    # healed rate >= 90% [0]
-    # healed rate >= 80% [1]
-    # healed rate >= 70% [2]
-    # healed rate >= 50% [3]
-    # healed rate < 50& [4]
-
-    # occupied bed ratio = occupied / (occupied + available)
-    # occupied bed ratio >= 40% need quarantine
 
 
 def scaleDeathRate(death_cases, _cases):
@@ -75,13 +78,9 @@ def scaleDeathRate(death_cases, _cases):
 
     # scaling death rate to fulfil conditions
     if defDeathRate <= 5:
-        defDeathRate = 0
+        defDeathRate = 5
     elif defDeathRate <= 10:
         defDeathRate = 10
-    elif defDeathRate <= 20:
-        defDeathRate = 20
-    elif defDeathRate <= 40:
-        defDeathRate = 40
     else:
         defDeathRate = 100
 
@@ -89,21 +88,42 @@ def scaleDeathRate(death_cases, _cases):
 
 
 def scaleHealedRate(healed_cases, hospitalized_cases):
-    defHealedRate = int(int(healed_cases)/int(hospitalized_cases))
+    defHealedRate = int(int(healed_cases)/int(hospitalized_cases) * 100)
 
     # scaling healed rate to fulfil conditions
     if defHealedRate >= 90:
         defHealedRate = 90
-    elif defHealedRate >= 80:
-        defHealedRate = 80
-    elif defHealedRate >= 70:
-        defHealedRate = 70
-    elif defHealedRate >= 50:
-        defHealedRate = 50
+    elif defHealedRate >= 75:
+        defHealedRate = 75
+    elif defHealedRate >= 40:
+        defHealedRate = 40
     else:
         defHealedRate = 0
 
     return defHealedRate
+
+
+def getAvailableBedRatio(available_beds, occupied_beds):
+    defAvailableBedRatio = int(int(available_beds)/(int(available_beds) + int(occupied_beds)))
+
+    if defAvailableBedRatio >= 90:
+        defAvailableBedRatio = 90
+    elif defAvailableBedRatio >= 75:
+        defAvailableBedRatio = 75
+    elif defAvailableBedRatio >= 30:
+        defAvailableBedRatio = 30
+    else:
+        defAvailableBedRatio = 0
+
+    return defAvailableBedRatio
+
+
+def getDailyStatus(daily_new_cases, available_bed):
+    defDailyStatus = 0
+    if int(available_bed) >= int(daily_new_cases):
+        defDailyStatus = 1
+
+    return defDailyStatus
 
 
 # Press the green button in the gutter to run the script.
@@ -111,13 +131,32 @@ if __name__ == '__main__':
     engine = QuarantineClassifier()
     engine.reset()
 
+    # getting inputs
     cases = input("Number of Cases: ")
     hospitalizedCases = input("Number of Hospitalized: ")
     healedCases = input("Number of Healed: ")
     deathCases = input("Number of Deaths: ")
+    newDailyCases = input("Number of Today's New Cases: ")
+    availableBeds = input("Number of Available Hospital Beds: ")
+    occupiedBeds = input("Number of Occupied Hospital Beds: ")
 
+    # get scaled inputs, ratio, and status
     healedRate = scaleHealedRate(healedCases, hospitalizedCases)
     deathRate = scaleDeathRate(deathCases, cases)
+    availableBedRatio = getAvailableBedRatio(availableBeds, occupiedBeds)
+    dailyStatus = getDailyStatus(newDailyCases, availableBeds)
 
-    engine.declare(Cases(deathRate=deathRate, healedRate=healedRate))
+    newDailyCases = int(newDailyCases)
+
+    # debugging process if needed
+    # print("healed rate:{healedRate}".format(healedRate=healedRate))
+    # print("death rate:{deathRate}".format(deathRate=deathRate))
+    # print("available bed ratio:{availableBedRatio}".format(availableBedRatio=availableBedRatio))
+    # print("daily status:{dailyStatus}".format(dailyStatus=dailyStatus))
+
+    engine.declare(
+        Cases(deathRate=deathRate, healedRate=healedRate),
+        NewDailyCases(status=dailyStatus, count=newDailyCases),
+        AvailableHospitalBed(availableRatio=availableBedRatio)
+    )
     engine.run()
